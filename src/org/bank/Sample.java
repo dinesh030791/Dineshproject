7ca112380b27b3c4f3902168b491ad6e8cfd6854
Vinoths92 <vinothsja5@gmail.com>
2019-02-28 13:48:07 +0530

    Updated

--- src/org/bank/Sample.java
+++ src/org/bank/Sample.java
@@ -3,6 +3,9 @@ package org.bank;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Naveen");
+		System.out.println("Vinoth");
+		System.out.println("Ramesh");
+		
 	}
 
 }
